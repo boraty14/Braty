3d4d46fe62a91923f53346ada4b73549065365ee
boraty14 <bratygame14@gmail.com>
2025-04-10 16:30:04 +0300

    rootcamera update

--- Assets/Braty/Core/Runtime/Scripts/Cameras/RootCamera.cs
+++ Assets/Braty/Core/Runtime/Scripts/Cameras/RootCamera.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 namespace Braty.Core.Runtime.Scripts.Cameras
 {
-    public class RootCamera : MonoBehaviour, IRootCamera
+    public class RootCamera : IRootCamera
     {
         private readonly RootCameraBehaviour _rootCameraBehaviour;
 
         public RootCamera()
         {
-            _rootCameraBehaviour = Instantiate(Resources.Load<RootCameraBehaviour>("RootCameraBehaviour"));
+            _rootCameraBehaviour = Object.Instantiate(Resources.Load<RootCameraBehaviour>("RootCameraBehaviour"));
         }
 
         public Camera Camera => _rootCameraBehaviour.Camera;
