d2fd38f036d6dfb504aae770b29cca73a497274f
boraty14 <bratygame14@gmail.com>
2025-07-28 07:51:24 +0300

    monopool update

--- Assets/Braty/Core/Runtime/Scripts/Pool/MonoPool.cs
+++ Assets/Braty/Core/Runtime/Scripts/Pool/MonoPool.cs
@@ -11,9 +11,25 @@ namespace Braty.Core.Runtime.Scripts.Pool
     public static class MonoPool
     {
         private static readonly Dictionary<Type, object> _monoPoolObjects = new();
+        private static readonly Dictionary<Type, object> _activeMonos = new();
 
-        public static void Init() => _monoPoolObjects.Clear();
+        public static void Init()
+        {
+            _monoPoolObjects.Clear();
+            _activeMonos.Clear();
+        }
 
+        public static IReadOnlyList<T> GetActiveMonos<T>() where T : MonoBehaviour
+        {
+            var monoType = typeof(T);
+            if (!_activeMonos.ContainsKey(monoType))
+            {
+                _activeMonos.TryAdd(monoType, new List<T>());
+            }
+
+            return (IReadOnlyList<T>)_activeMonos[monoType];
+        }
+        
         public static void LoadPool<T>(Transform poolParent, Action onLoaded = null, int initial = 10, int max = 100, bool collectionChecks = false) where T : MonoBehaviour
         {
             var monoPoolObjectKey = typeof(T);
@@ -61,13 +77,48 @@ namespace Braty.Core.Runtime.Scripts.Pool
         public static T Get<T>() where T : MonoBehaviour
         {
             var monoPoolObject = (MonoPoolDefinition<T>)_monoPoolObjects[typeof(T)];
-            return monoPoolObject.Pool.Get();
+            var mono = monoPoolObject.Pool.Get();
+            AddActiveMono(mono);
+            return mono;
+        }
+        
+        private static void AddActiveMono<T>(T monoInstance) where T : MonoBehaviour
+        {
+            var monoType = typeof(T);
+            if (!_activeMonos.ContainsKey(monoType))
+            {
+                _activeMonos.TryAdd(monoType, new List<T>());
+            }
+            
+            ((List<T>)_activeMonos[monoType]).Add(monoInstance);
         }
 
         public static void Release<T>(T obj) where T : MonoBehaviour
         {
             var monoPoolObject = (MonoPoolDefinition<T>)_monoPoolObjects[typeof(T)];
             monoPoolObject.Pool.Release(obj);
+            RemoveActiveMono(obj);
+        }
+        
+        private static void RemoveActiveMono<T>(T monoInstance) where T : MonoBehaviour
+        {
+            var monoType = typeof(T);
+            if (!_activeMonos.ContainsKey(monoType))
+            {
+                Debug.LogError($"Mono list {monoType} is empty");
+                return;
+            }
+
+            var monoList = (List<T>)_activeMonos[monoType];
+            int monoIndex = monoList.IndexOf(monoInstance);
+            if (monoIndex < 0)
+            {
+                Debug.LogError($"Mono is not in list {monoType}");
+                return;
+            }
+
+            monoList[monoIndex] = monoList[^1];
+            monoList.RemoveAt(monoList.Count - 1);
         }
 
         private static T CreateSetup<T>() where T : MonoBehaviour
