36aeacc449b3dcb296cc354ad766ee854b9414d7
boraty14 <bratygame14@gmail.com>
2025-08-09 00:05:27 +0300

    add right click

---
+++ Assets/Braty/Core/Runtime/Scripts/BUI/Core/BRightClickable.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Braty.Core.Runtime.Scripts.BUI.Core
+{
+    public class BRightClickable : BInteractable
+    {
+        public override void RightClickEvent(Vector2 mousePosition)
+        {
+            base.RightClickEvent(mousePosition);
+        }
+    }
+}
